3a164226e041bd5f1a49bc06b65c9bda9841bf5d
TatBat <tat.batishcheva@gmail.com>
2019-02-17 11:19:47 +0300

    Inline If-Else with Conditional Operator

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Mailbox(props) {
-    const unreadMessages = props.unreadMessages;
-    return (
-      <div>
-        <h1>Hello!</h1>
-        {unreadMessages.length > 0 &&
-          <h2>
-            You have {unreadMessages.length} unread messages.
-          </h2>
-        }
-      </div>
-    );
-  }
-  
-  //const messages = ['React', 'Re: React', 'Re:Re: React', 'Re:Re:Re: React'];
-  const messages = [];
-  ReactDOM.render(
-    <Mailbox unreadMessages={messages} />,
-    document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
