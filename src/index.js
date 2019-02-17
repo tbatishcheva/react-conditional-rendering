2ccc41e712e8d5dea2922931841538c75444e246
TatBat <tat.batishcheva@gmail.com>
2019-02-17 11:31:31 +0300

    Preventing Component from Rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+function WarningBanner(props) {
+    if (!props.warn) {
+      return null;
+    }
+  
+    return (
+      <div className="warning">
+        Warning!
+      </div>
+    );
+  }
+  
+  class Page extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {showWarning: true};
+      this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+  
+    handleToggleClick() {
+      this.setState(state => ({
+        showWarning: !state.showWarning
+      }));
+    }
+  
+    render() {
+      return (
+        <div>
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handleToggleClick}>
+            {this.state.showWarning ? 'Hide' : 'Show'}
+          </button>
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Page />,
+    document.getElementById('root')
+  );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
